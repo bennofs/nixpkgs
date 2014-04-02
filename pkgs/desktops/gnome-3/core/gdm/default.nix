f98110d289867b26a801507a796864dd666cc021
Luca Bruno <luca.bruno@immobiliare.it>
2014-04-02 18:21:00 +0200

    gdm: Update and fix gsettings gschemas

--- pkgs/desktops/gnome-3/core/gdm/default.nix
+++ pkgs/desktops/gnome-3/core/gdm/default.nix
@@ -1,18 +1,39 @@
-{ stdenv, fetchurl, pkgconfig, glib, itstool, libxml2, intltool, accountservice, libX11
+{ stdenv, fetchurl, pkgconfig, glib, itstool, libxml2, xorg
+, intltool, accountservice, libX11, autoreconfHook, gnome3, systemd
 , gtk, libcanberra_gtk3, pam, libtool, gobjectIntrospection }:
 
 stdenv.mkDerivation rec {
-  name = "gdm-3.10.0.1";
+  name = "gdm-3.12.0";
 
   src = fetchurl {
-    url = "mirror://gnome/sources/gdm/3.10/${name}.tar.xz";
-    sha256 = "1rva3djas48m8w1gyv3nds3jxfkirdfl0bk30x79mizrk80456jl";
+    url = "mirror://gnome/sources/gdm/3.12/${name}.tar.xz";
+    sha256 = "74889819a0c55577fd04236b5308de5f1fcf1992b221c71d02d91a1e564a5d87";
   };
 
-  buildInputs = [ pkgconfig glib itstool libxml2 intltool accountservice
-                  gobjectIntrospection libX11 gtk libcanberra_gtk3 pam libtool ];
+  preAutoreconf = ''
+    # GDM hardcodes the xserver path
+    substituteInPlace ./configure.ac --replace "/usr/bin/X" "/var/lib/gdm/xserver-wrapper"
+
+    # https://bugzilla.gnome.org/show_bug.cgi?id=725761
+    substituteAllInPlace ./configure.ac --replace "AC_SUBST" "AS_AC_EXPAND"
+
+    # Don't install a systemd service
+    substituteInPlace ./data/Makefile.am --replace "systemdsystemunit += gdm.service" ""
+
+    # Hardcoded pam check
+    substituteInPlace ./data/Makefile.am --replace "/usr/include/security/pam_appl.h" "${pam}/include/security/pam_appl.h"
+  '';
+
+  configureFlags = [ "--with-systemd" "--with-default-pam-config=lfs" "--localstatedir=/var" ];
+
+  # autoreconfHook needed to apply the expandvars patch
+  buildInputs = [ pkgconfig glib itstool libxml2 intltool
+                  accountservice autoreconfHook gnome3.dconf
+                  gobjectIntrospection libX11 gtk systemd
+                  libcanberra_gtk3 pam libtool ];
 
   meta = with stdenv.lib; {
+    homepage = https://wiki.gnome.org/Projects/GDM;
     platforms = platforms.linux;
   };
 }
